e4f48d669a6c05ec7e33bd40cc49a8b10345b5e6
Rafał Wojnecki <rafal.wojnecki@gmail.com>
2021-07-19 21:11:23 +0200

    Add option to retry on error, debounce, fix duplicate requests when rotating

--- app/src/main/java/com/example/githubreposearcher/feature/reposearch/RepoSearchFragment.kt
+++ app/src/main/java/com/example/githubreposearcher/feature/reposearch/RepoSearchFragment.kt
@@ -1,56 +1,43 @@
 package com.example.githubreposearcher.feature.reposearch
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.githubreposearcher.R
-import com.example.githubreposearcher.common.applySchedulers
 import com.example.githubreposearcher.databinding.FragmentRepoSearchBinding
 import com.example.githubreposearcher.domain.Result
 import com.example.githubreposearcher.domain.model.GitHubRepo
 import dagger.hilt.android.AndroidEntryPoint
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.rxkotlin.plusAssign
 
 @AndroidEntryPoint
 class RepoSearchFragment : Fragment(R.layout.fragment_repo_search) {
 
     private val viewModel: RepoSearchViewModel by viewModels()
-    private val disposable = CompositeDisposable()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentRepoSearchBinding.bind(view)
-
-        setupSearchBar(binding)
-        setupReposList(binding)
+        setupViews(binding)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        disposable.clear()
-    }
-
-    private fun setupSearchBar(binding: FragmentRepoSearchBinding) = with(binding) {
+    private fun setupViews(binding: FragmentRepoSearchBinding) = with(binding) {
         searchInput.editText?.doOnTextChanged { text, _, _, _ ->
             viewModel.onSearchQueryChanged(text?.toString() ?: "")
         }
-    }
+        errorView.btnRetry.setOnClickListener { viewModel.onRetryClick() }
 
-    private fun setupReposList(binding: FragmentRepoSearchBinding) = with(binding) {
-        binding.searchResultsRv.adapter = RepoSearchAdapter()
+        searchResultsRv.adapter = RepoSearchAdapter()
 
-        disposable += viewModel.repositoriesList
-            .applySchedulers()
-            .subscribe { renderState(binding, it) }
+        viewModel.repositoriesLiveData().observe(viewLifecycleOwner) { renderState(binding, it) }
     }
 
     private fun renderState(binding: FragmentRepoSearchBinding, result: Result<List<GitHubRepo>>) = with(binding) {
-        searchResultsGroup.isVisible = false
+        val stateViews = listOf(loadingView.root, errorView.root, emptyView.root, searchResultsRv)
+        stateViews.forEach { it.isVisible = false }
+
         when (result) {
             Result.Loading -> {
                 loadingView.root.isVisible = true
--- app/src/main/java/com/example/githubreposearcher/feature/reposearch/RepoSearchViewModel.kt
+++ app/src/main/java/com/example/githubreposearcher/feature/reposearch/RepoSearchViewModel.kt
@@ -1,22 +1,55 @@
 package com.example.githubreposearcher.feature.reposearch
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.githubreposearcher.common.toResult
 import com.example.githubreposearcher.domain.GitHubRepository
+import com.example.githubreposearcher.domain.Result
+import com.example.githubreposearcher.domain.model.GitHubRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.plusAssign
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltViewModel
 class RepoSearchViewModel @Inject constructor(productsRepository: GitHubRepository) : ViewModel() {
 
-    private val searchQuery: BehaviorSubject<String> = BehaviorSubject.create()
+    private val disposable = CompositeDisposable()
 
-    val repositoriesList = searchQuery
-        .switchMap { query -> productsRepository.getGithubRepositories(query).toResult() }
+    private val searchQuery: BehaviorSubject<String> = BehaviorSubject.create()
+    private val retry: PublishSubject<Unit> = PublishSubject.create()
+
+    private val repositoriesLiveData = MutableLiveData<Result<List<GitHubRepo>>>()
+
+    init {
+        disposable += searchQuery
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .filter { it.length > 3 }
+            .doOnNext { repositoriesLiveData.postValue(Result.Loading) }
+            .switchMapSingle { query -> productsRepository.getGithubRepositories(query) }
+            .doOnError { repositoriesLiveData.postValue(Result.Error(it)) }
+            .retryWhen { it.flatMap { retry } }
+            .subscribeBy { repositoriesLiveData.postValue(Result.Success(it)) }
+    }
+
+    public override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
+
+    fun repositoriesLiveData(): LiveData<Result<List<GitHubRepo>>> = repositoriesLiveData
 
     fun onSearchQueryChanged(query: String) {
         searchQuery.onNext(query)
+    }
+
+    fun onRetryClick() {
+        retry.onNext(Unit)
     }
 }
 
