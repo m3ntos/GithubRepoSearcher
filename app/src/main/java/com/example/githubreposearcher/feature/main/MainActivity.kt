09076a351187bcf4da414a8a810a4d9a99be9125
Rafał Wojnecki <rafal.wojnecki@gmail.com>
2021-07-19 17:31:17 +0200

    Add navigation

---
+++ app/src/main/java/com/example/githubreposearcher/feature/main/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.githubreposearcher.feature.main
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.githubreposearcher.R
+
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main)
---
+++ app/src/main/java/com/example/githubreposearcher/feature/reposearch/RepoSearchFragment.kt
@@ -0,0 +1,7 @@
+package com.example.githubreposearcher.feature.reposearch
+
+import androidx.fragment.app.Fragment
+import com.example.githubreposearcher.R
+
+class RepoSearchFragment: Fragment(R.layout.fragment_repo_search) {
+}
